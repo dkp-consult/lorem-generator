44fc1e1e3ab22c9788e3de57df242422147148ed
dkp-consult <contact@dkp-consult.be>
2024-03-06 17:51:53 +0100

    dev: work on design

--- src/quizz.js
+++ src/quizz.js
@@ -8,12 +8,22 @@ function ajouterQuestion() {
   questionCount++;
   const encodingSection = document.getElementById("encodingSection");
   const questionHTML = `
-        <div class="questionBlock">
-            <div>Question ${questionCount}: <input type="text" placeholder="Votre question ici" id="question${questionCount}"></div>
-            <div>Réponse 1: <input type="text" class="reponse${questionCount}"><input type="radio" name="correct${questionCount}" value="1"></div>
-            <div>Réponse 2: <input type="text" class="reponse${questionCount}"><input type="radio" name="correct${questionCount}" value="2"></div>
-            <div>Réponse 3: <input type="text" class="reponse${questionCount}"><input type="radio" name="correct${questionCount}" value="3"></div>
-        </div>
+  <div class="questionBlock bg-white p-4 rounded-lg mb-4">
+  <div class="font-bold mb-2">Question ${questionCount}: <input type="text" placeholder="Votre question ici" id="question${questionCount}" class="input bg-gray-100 rounded border border-gray-300 p-2 focus:outline-none focus:border-blue-500 w-full"></div>
+  <div class="flex items-center mb-2">
+    <input type="text" class="reponse${questionCount} bg-gray-100 rounded border border-gray-300 p-2 focus:outline-none focus:border-blue-500 mr-2 w-full" placeholder="Réponse 1">
+    <input type="radio" name="correct${questionCount}" value="1" class="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300">
+  </div>
+  <div class="flex items-center mb-2">
+    <input type="text" class="reponse${questionCount} bg-gray-100 rounded border border-gray-300 p-2 focus:outline-none focus:border-blue-500 mr-2 w-full" placeholder="Réponse 2">
+    <input type="radio" name="correct${questionCount}" value="2" class="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300">
+  </div>
+  <div class="flex items-center mb-2">
+    <input type="text" class="reponse${questionCount} bg-gray-100 rounded border border-gray-300 p-2 focus:outline-none focus:border-blue-500 mr-2 w-full" placeholder="Réponse 3">
+    <input type="radio" name="correct${questionCount}" value="3" class="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300">
+  </div>
+</div>
+
         <br>`;
   encodingSection.insertAdjacentHTML("beforeend", questionHTML);
 }
@@ -30,9 +40,8 @@ function genererPreview() {
     const correctInput = block.querySelector(`input[name='correct${index + 1}']:checked`).value;
     correctAnswersScript += `    "${questionNumber}": "${correctInput}",\n`;
 
-    let previewHTML = `<div class="question">Question ${
-      index + 1
-    }: ${question}</div>`;
+    let previewHTML = `<div class="question">Question ${index + 1
+      }: ${question}</div>`;
     reponses.forEach((reponse, idx) => {
       previewHTML += `<div><input type="radio" name="${questionNumber}" value="${idx + 1}"> ${reponse.value}</div>`;
     });
@@ -80,7 +89,7 @@ function correction() {
   codeOutput.value = totalPreviewHTML + correctionScript;
 }
 
-document.getElementById('btnGenerer').addEventListener('click', function() {
+document.getElementById('btnGenerer').addEventListener('click', function () {
   const codeOutput = document.getElementById("codeOutput").value;
   genererPreview(); // Génère le contenu pour codeOutput
   insererContenuDansIframe(codeOutput); // Insère le contenu de codeOutput dans l'iframe
@@ -94,41 +103,41 @@ function copierCode() {
     .catch((err) => console.error("Erreur lors de la copie :", err));
 }
 
-  
 
-  function insererContenuDansIframe(code) {
-    // Obtenir le parent de l'iframe actuelle
-    const iframeContainer = document.getElementById('vueLive').parentNode;
-    const oldIframe = document.getElementById('vueLive');
-  
-    // Créer une nouvelle iframe
-    const newIframe = document.createElement('iframe');
-    newIframe.id = oldIframe.id;
-    newIframe.name = oldIframe.name;
-    newIframe.style.width = oldIframe.style.width; // Copiez d'autres attributs au besoin
-    newIframe.style.height = oldIframe.style.height;
-  
-    // Supprimer l'ancienne iframe et ajouter la nouvelle au conteneur
-    iframeContainer.removeChild(oldIframe);
-    iframeContainer.appendChild(newIframe);
-  
-    // Utiliser la nouvelle iframe pour le contenu
-    const iframe = newIframe;
-    const doc = iframe.contentDocument || iframe.contentWindow.document;
-  
-    // Procédez comme avant pour insérer le contenu
-    doc.open();
-    doc.write("..");
-    doc.close();
-  
-    const scriptRegex = /<script.*?>([\s\S]*?)<\/script>/gi;
-    let scriptContent = '';
-    let htmlContent = code.replace(scriptRegex, function(match, group1) {
-      scriptContent += group1 + '\n';
-      return ''; // Supprime le script du HTML
-    });
-  
-    const contenu = `
+
+function insererContenuDansIframe(code) {
+  // Obtenir le parent de l'iframe actuelle
+  const iframeContainer = document.getElementById('vueLive').parentNode;
+  const oldIframe = document.getElementById('vueLive');
+
+  // Créer une nouvelle iframe
+  const newIframe = document.createElement('iframe');
+  newIframe.id = oldIframe.id;
+  newIframe.name = oldIframe.name;
+  newIframe.style.width = oldIframe.style.width; // Copiez d'autres attributs au besoin
+  newIframe.style.height = oldIframe.style.height;
+
+  // Supprimer l'ancienne iframe et ajouter la nouvelle au conteneur
+  iframeContainer.removeChild(oldIframe);
+  iframeContainer.appendChild(newIframe);
+
+  // Utiliser la nouvelle iframe pour le contenu
+  const iframe = newIframe;
+  const doc = iframe.contentDocument || iframe.contentWindow.document;
+
+  // Procédez comme avant pour insérer le contenu
+  doc.open();
+  doc.write("..");
+  doc.close();
+
+  const scriptRegex = /<script.*?>([\s\S]*?)<\/script>/gi;
+  let scriptContent = '';
+  let htmlContent = code.replace(scriptRegex, function (match, group1) {
+    scriptContent += group1 + '\n';
+    return ''; // Supprime le script du HTML
+  });
+
+  const contenu = `
       <!DOCTYPE html>
       <html lang="en">
       <head>
@@ -140,21 +149,28 @@ function copierCode() {
       </body>
       </html>
     `;
-  
-    doc.open();
-    doc.write(contenu);
-  
-    if (scriptContent.trim() !== '') {
-      const scriptTag = doc.createElement("script");
-      scriptTag.textContent = scriptContent;
-      doc.body.appendChild(scriptTag);
-    }
-  
-    doc.close();
-  }
-  
-  document.getElementById('btnGenerer').addEventListener('click', function() {
-    const codeOutput = document.getElementById("codeOutput").value;
-    insererContenuDansIframe(codeOutput);
-  });
 
+  doc.open();
+  doc.write(contenu);
+
+  if (scriptContent.trim() !== '') {
+    const scriptTag = doc.createElement("script");
+    scriptTag.textContent = scriptContent;
+    doc.body.appendChild(scriptTag);
+  }
+
+  doc.close();
+}
+
+document.getElementById('btnGenerer').addEventListener('click', function () {
+  const codeOutput = document.getElementById("codeOutput").value;
+  insererContenuDansIframe(codeOutput);
+});
+
+document.getElementById('openModal').addEventListener('click', function() {
+  document.getElementById('modal').classList.remove('hidden');
+});
+
+document.getElementById('closeModal').addEventListener('click', function() {
+  document.getElementById('modal').classList.add('hidden');
+});
